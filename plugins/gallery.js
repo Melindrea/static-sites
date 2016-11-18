7e6fd97eaf1a73d183d64c4ce52ea5ad2fbf2b18
Marie Hogebrandt <iam@mariehogebrandt.se>
2016-11-18 07:39:34 +0100

    Move gallery to use IDs rather than full image objects

--- plugins/gallery.js
+++ plugins/gallery.js
@@ -8,24 +8,28 @@ function plugin(name) {
     return function(app) {
         var files = app.getViews(name),
             config = require('./../config'),
-            gallery, size, items = [], sizes;
+            gallery, size, items = [], sizes, images, image;
 
         for (var file in files) {
             if (files[file].data.gallery) {
                 gallery = config.media.galleries[files[file].data.gallery];
                 size = gallery.size,
-                sizes = config.media.sizes;
+                sizes = config.media.sizes,
+                images = config.media.images;
 
-                gallery.items.forEach(function (item) {
-                    var newItem = {
-                        id: item.id,
-                        alt: item.alt,
-                        caption: item.caption,
-                        url: sizes[size] + item.src,
-                        thumbnailUrl: sizes.thumbnail + item.src
-                    };
+                gallery.items.forEach(function (imageId) {
+                    if (imageId in images) {
+                        image = images[imageId];
+                        var newItem = {
+                            id: imageId,
+                            alt: image.alt,
+                            caption: image.caption,
+                            url: sizes[size] + image.src,
+                            thumbnailUrl: sizes.thumbnail + image.src
+                        };
 
-                    items.push(newItem);
+                        items.push(newItem);
+                    }
                 });
 
                 files[file].data.gallery = items;
